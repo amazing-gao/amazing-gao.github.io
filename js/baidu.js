3b6fff82ecdd0679a73d26bd6b52336dce02f77f
amazing-gao <amazing-gao@users.noreply.github.com>
2020-11-14 08:28:52 +0000

    deploy: 042dfe3ecea0cc82f0e8dd2a057fa1db9000091c

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?815a521e3c0aee7749c5f8350acb8c6d";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
